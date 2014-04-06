66491e914b1ec4a8afc12766605586fe671d6a7c
Toby Retallick <toby.retallick@gmail.com>
2014-04-06 19:43:34 +0100

    Fixed nav to top

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,11 +1,11 @@
 $('#tweet_input').focus(function()
 {
     /*to make this flexible, I'm storing the current width in an attribute*/
-    $(this).attr('data-default', $(this).width());
+    $(this).attr('data-default', $(this).height());
     $(this).animate({ height: 100 }, 'fast');
 }).blur(function()
 {
-    /* lookup the original width */
-    var w = $(this).attr('data-default');
-    $(this).animate({ width: w }, 'slow');
+    /* lookup the original height */
+    var h = $(this).attr('data-default');
+    $(this).animate({ height: h }, 'fast');
 });
