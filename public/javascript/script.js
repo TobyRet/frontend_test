1303b8f2e5832c2bf3b1f328e0a0c8dfd093efbe
Toby Retallick <toby.retallick@gmail.com>
2014-04-07 09:20:02 +0100

    Added infinite scroll

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,3 +1,22 @@
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+
+        setTimeout(function() {
+            $('.loader').css('display', 'initial');
+        }, 1000);
+
+        setTimeout(function() {
+            $('.hidden_tweet').css('display', 'initial');
+
+        }, 2000);
+
+        setTimeout(function() {
+            $('.loader').css('display', 'none');
+        }, 2000);
+    }
+});
+
+
 $(".compose_tweet").focusin(function() {
     $(".compose_extra_buttons").show();
     $("#tweet_input").animate({ height: 100 }, 'fast');
@@ -5,3 +24,4 @@ $(".compose_tweet").focusin(function() {
     $(".compose_extra_buttons").hide();
     $("#tweet_input").animate({ height: 30 }, 'fast');
 });
+
